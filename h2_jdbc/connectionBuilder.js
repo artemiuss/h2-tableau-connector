5031dbd0903ecf5a1c50713b28b3d7ef5d4bedb7
artemiuss <artemiustas@gmail.com>
2021-11-05 02:19:02 +0200

    Add files via upload

--- h2_jdbc/connectionBuilder.js
+++ h2_jdbc/connectionBuilder.js
@@ -1,5 +1,5 @@
 (function dsbuilder(attr) {
-    var urlBuilder = "jdbc:h2:tcp//" + attr[connectionHelper.attributeServer] + ":" + attr[connectionHelper.attributePort] + "/" + attr[connectionHelper.attributeDatabase] + "?";
+    var urlBuilder = "jdbc:h2:tcp://" + attr[connectionHelper.attributeServer] + ":" + attr[connectionHelper.attributePort] + "/" + attr[connectionHelper.attributeDatabase];
 
     return [urlBuilder];
 })
